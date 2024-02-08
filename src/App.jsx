7bb5fafca510c85274c515a520f2d46c06cb4e83
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-02-09 00:04:20 +0530

    Createing routes or pages

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signinn from './pages/Signinn'
+
+
+const App = () => {
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About />} />
+    <Route path='/sign-in' element={<Signinn />} />
+    <Route path='/sign-up' element={<SignUp />} />
+    <Route path='/dashboard' element={<Dashboard/>} />
+    <Route path='/projects' element={<Projects/>} />
+
+
+
+
+   </Routes>
+   
+   
+   </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Signinn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signinn = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Signinn
