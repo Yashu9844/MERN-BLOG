df43e71af343e1020634423caf8060278a45e8d5
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-02-11 03:09:56 +0530

    create  signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,30 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'
+
+export const signup = async (req,res)=>{
+    const {username,email,password} = req.body;
+
+    if(!username || !email || !password || username === ''|| email === ''|| password === ''){
+        return res.status(400).json({message:'All fileds are required'});
+    }
+
+    
+
+   const hashedPassword = bcryptjs.hashSync(password,10)
+    const hashedEmail = bcryptjs.hashSync(email,10)
+
+
+    const newUser = new User({
+        username,
+        email:hashedEmail  ,                                          //extra feature to hash the  email in Database
+        password:hashedPassword
+    })
+
+    try{
+        await newUser.save();
+    res.json('Successfully singed in')
+    }
+    catch(error){
+    res.status(500).json({message:error.message})
+    }
+}
