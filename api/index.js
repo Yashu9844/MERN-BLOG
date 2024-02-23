8f93b3c8b3937358a2fe69a7e66687fe09243e20
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-02-23 23:48:44 +0530

    Add update user api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose'; // Fix the typo here
 import dotenv from 'dotenv';
-import useRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import useRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+
+
 
 
 dotenv.config();
@@ -19,6 +22,7 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!!');
