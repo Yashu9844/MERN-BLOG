9fd703f65fd175537aaf284538ef2af24eddb9f0
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-03-01 02:01:48 +0530

    addd signout functionality to header and sidebar

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -3,8 +3,11 @@ import {Sidebar, SidebarItem, SidebarItemGroup} from 'flowbite-react'
 import {HiUser,HiArrowSmRight} from 'react-icons/hi'
 import { useEffect,useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import { signoutSuccess } from '../redux/user/userSlice'
+import {  useDispatch } from 'react-redux'
 
 const DashSidebar = () => {
+  const dispatch = useDispatch()
     const [tab,setTab] = useState('')
   const location = useLocation()
 
@@ -16,6 +19,21 @@ const DashSidebar = () => {
       
     }
   },[location.search])
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
    <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
@@ -24,7 +42,7 @@ const DashSidebar = () => {
                 <SidebarItem active={tab === 'profile'} icon={HiUser} label={"User"} labelColor="dark" as='div'>
                     Profile
                 </SidebarItem>
-                <SidebarItem  icon={HiArrowSmRight} classnam>
+                <SidebarItem  icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
                     Sign Out
                 </SidebarItem>
                 </Link>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ import{FaMoon,FaSun} from 'react-icons/fa'
 import { useLocation } from 'react-router-dom';
 import { useSelector,useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
-
+import { signoutSuccess } from '../redux/user/userSlice';
 
 
 const Header = () => {
@@ -14,6 +14,25 @@ const Header = () => {
   const path = useLocation().pathname
   const {currentUser} = useSelector(state => state.user)
   const {theme} = useSelector(state => state.theme)
+
+
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
   <Navbar className='border-b-2'>
  <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -91,7 +110,7 @@ const Header = () => {
       </DropdownItem>
      </Link>
      <Dropdown.Divider/>
-     <DropdownItem>
+     <DropdownItem onClick={handleSignout}>
       Sign Out
      </DropdownItem>
 
