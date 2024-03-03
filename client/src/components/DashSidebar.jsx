4e82924ea378aa27d6571cb2486c1a3a3c171a20
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-03-03 21:22:49 +0530

    add posts section  DashBord

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import {Sidebar, SidebarItem, SidebarItemGroup} from 'flowbite-react'
-import {HiUser,HiArrowSmRight} from 'react-icons/hi'
-import { useEffect,useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import { signoutSuccess } from '../redux/user/userSlice'
-import {  useDispatch } from 'react-redux'
+import { Sidebar } from 'flowbite-react';
 
-const DashSidebar = () => {
-  const dispatch = useDispatch()
-    const [tab,setTab] = useState('')
-  const location = useLocation()
+import { HiUser, HiArrowSmRight, HiDocumentText } from 'react-icons/hi';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-  useEffect(()=>{
+export default function DashSidebar() {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const [tab, setTab] = useState('');
+  useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFormURL = urlParams.get('tab')
-    if (tabFormURL) {
-      setTab(tabFormURL)
-      
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
     }
-  },[location.search])
+  }, [location.search]);
   const handleSignout = async () => {
     try {
       const res = await fetch('/api/user/signout', {
@@ -35,23 +35,42 @@ const DashSidebar = () => {
     }
   };
   return (
-   <Sidebar className='w-full md:w-56'>
-        <Sidebar.Items>
-            <Sidebar.ItemGroup>
-                <Link to='/dashboard?tab=profile'>
-                <SidebarItem active={tab === 'profile'} icon={HiUser} label={"User"} labelColor="dark" as='div'>
-                    Profile
-                </SidebarItem>
-                <SidebarItem  icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
-                    Sign Out
-                </SidebarItem>
-                </Link>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
+    <Sidebar className='w-full md:w-56'>
+      <Sidebar.Items>
 
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
+          <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
 
-   </Sidebar>
-  )
+              label={currentUser.isAdmin ? 'Admin' : 'User'}
+              labelColor='dark'
+              as='div'
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=posts'>
+              <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiDocumentText}
+                as='div'
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className='cursor-pointer'
+            onClick={handleSignout}
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
 }
-
-export default DashSidebar
