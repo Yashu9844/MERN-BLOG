94227add454b14539bb85445439323cf010936b0
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-03-06 23:55:59 +0530

    update post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -71,6 +71,8 @@ res.status(200).json({
 
 
 
+
+
   } catch (error) {
     next(error)
     
@@ -93,3 +95,26 @@ export const  deletepost = async(req,res,next)=>{
  }
 
 }
+
+export const updatepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this post'));
+  }
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.category,
+          image: req.body.image,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
+};
