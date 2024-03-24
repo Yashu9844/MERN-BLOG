e3f9560c30e5873739fb4507ab74f33b9fa99ce5
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-03-24 17:14:49 +0530

    add about and project section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h1>I am about</h1>
+    <div className='min-h-screen flex justify-center items-center'>
+      <div className="max-w-2xl mx-auto p-3  text-center" >
+        <div className="">
+          <h1 className='text-4xl font-semibold text-center my-7'>About Yashwanth's Blog</h1>
+          <div className=" flex flex-col gap-6 p-4 m text-md text-gray-600">
+            <p>
+            Welcome to Yashwanth's Blog! This blog was created by Yashwanth as a personal project to share his thoughts and ideas with the world. yashwanth is a  passionate developer who loves to code,learn new tech and everything in between.
+
+          
+            </p>
+            <p>
+            On this blog, you'll find  articles and tutorials on topics such as web development, software engineering, and programming languages. Yashwanth is always learning and exploring new technologies, so be sure to check back often for new content!
+            </p>
+            <p>
+            I encourage you to leave comments on my posts and engage with other readers. You can like other people's comments and reply to them as well. I believe that a community of learners can help each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction'
 
 const Projects = () => {
   return (
-    <div>
-        <h1>I am projects</h1>
+
+       <div className='min-h-screen flex flex-col gap-5 items-center justify-center'>
+       <h1 className='text-5xl text-center'> Projects</h1>
+       <p className='text-md text-center'>
+       Build fun and engaging projects while learning web development 
+       </p>
+       <CallToAction/>
+    
     </div>
   )
 }
